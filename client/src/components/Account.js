10ea0ffefcdd2b938d01af3b5b33b7a8a0c4e635
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2024-02-06 16:36:19 -0600

    created account.js page

---
+++ client/src/components/Account.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { redirect } from "react-router-dom";
+
+import Auth from "../utils/auth";
+
+const Account = () => {
+  console.log(Auth.hasUser());
+  return <div></div>;
+};
+
+export default Account;
